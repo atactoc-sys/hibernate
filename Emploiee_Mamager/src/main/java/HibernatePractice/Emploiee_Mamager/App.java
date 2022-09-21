88479f2a9c1f1410cde353462f5bb072dac79948
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-09-21 18:18:55 +0530

    Add files via upload

---
+++ Emploiee_Mamager/src/main/java/HibernatePractice/Emploiee_Mamager/App.java
@@ -0,0 +1,13 @@
+package HibernatePractice.Emploiee_Mamager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
